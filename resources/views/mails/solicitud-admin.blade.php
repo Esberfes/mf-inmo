264c3a7657a2025141659751c773fa335b680faa
Esberfes <casas222@hotmail.com>
2020-09-09 20:36:37 +0200

    Email a admin mejoras

--- resources/views/mails/solicitud-admin.blade.php
+++ resources/views/mails/solicitud-admin.blade.php
@@ -5,6 +5,6 @@
 <body>
     <h1>Solicitud de contacto: {{ $solicitud->local->titulo }}</h1>
     <p>Hola {{ $admin->nombre }} se ha realizado una nueva solicitud, puede atenderla desde el Area Privada</p>
-    <small>Id: $solicitud->id</small>
+    <small>Id: {{ $solicitud->id }}</small>
 </body>
 </html>
